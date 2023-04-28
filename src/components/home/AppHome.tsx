6a242f30bd80db27c7e10898e7deb34dee0c1d8d
Joie <joie.cosby@udemy.com>
2023-04-28 15:23:52 -0700

    Playing around with the initial App Home page

--- src/components/home/AppHome.tsx
+++ src/components/home/AppHome.tsx
@@ -1,8 +1,56 @@
+// import { Fragment } from 'react';
+// import { Link } from 'react-router-dom';
+
+const HOME_ITEMS = [
+    {
+        title: 'Query',
+        
+    },
+    {
+        title: 'Load',
+        
+    },
+];
+
+// const AppHome = () => {
+//     return(
+//         <h1>hello</h1>
+
+//         {HOME_ITEMS.map((card) => (
+//             <div
+//               key={card.title}
+//               className="slds-box slds-box_x-small"
+//             >
+//               <article className="slds-tile slds-media">
+
+//                 <div className="slds-media__body">
+//                   <h3
+//                     className="slds-text-title_caps"
+       
+//                   >
+//                     {card.title}
+//                   </h3>
+//                   </div>
+//               </article>
+//             </div>
+//         ))}
+//     );
+// };
+
 const AppHome = () => {
     return(
-        <h1>hello</h1>
+        
+        <div> {/* This is where I get the error message */}
+            <h1>POPULAR MOVIES</h1>
+                {   
+                    HOME_ITEMS?.map((card) => (
+                        <div>
+                            <p>{card.title}</p>
+                        </div>
+                    ))
+                }
+        </div>
     );
-}
-
+};
 
 export default AppHome;
