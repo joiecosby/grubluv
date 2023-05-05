b0ab20a07cb788b84a212e6ac5cb4f79a38333f1
Joie Cosby <jc283108@gmail.com>
2023-05-04 19:50:20 -0700

    adding TODO's

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,33 +2,33 @@ import AppHome from './home/AppHome';
 import Todo from './core/Todo';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function Product({ match }) {
+ return <h2>This is a page for product with ID: {match.params.id} </h2>;
+}
 
 const App = () => {
-  return (
-    <div>
-      <AppHome />
-    </div>
-    // <div>
-    //   <div
-    //     css={css({
-    //       margin: 10,
-    //       padding: 10,
-    //       backgroundColor: '#eee',
-    //     })}
-    //   >
-    //     This is an example of <code>`css`</code> using an object.
-    //   </div>
-    //   <div
-    //     css={css`
-    //       margin: 10px;
-    //       padding: 10px;
-    //       background-color: #235789;
-    //     `}
-    //   >
-    //     This is an example of <code>`css`</code> using a tagged template literal.
-    //   </div>
-    // </div>
-  );
+  // return (
+  //   <div>
+  //     <AppHome />
+  //   </div>
+  // );
+
+//https://levelup.gitconnected.com/lets-setup-a-react-typescript-project-with-material-ui-react-router-3d7ea8cb5596
+/**
+ * Instead of tabs, use NavBar. (look at HeaderNavBar in jetstream)
+ * Then create home page 
+ * create user sign up
+ * 
+ */
+  
 };
 
 export default App;
+
+ 
